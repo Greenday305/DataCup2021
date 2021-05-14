c7836007c51cb9bd60a6d22dfa69f03bf0677884
David <a00825273@itesm.mx>
2021-05-14 12:29:13 -0500

    David: Checando los titulos y una fila aleatoria

--- Vacunas.py
+++ Vacunas.py
@@ -3,3 +3,18 @@ from openpyxl import Workbook
 
 wb = load_workbook('vaccination_all_tweets.xlsx')
 sheet = wb.worksheets[0]
+
+tit = []
+
+for row in sheet.iter_rows(min_row=1,max_row=1,values_only=True):
+	tit += row
+
+print(tit)
+
+val = []
+
+for row in sheet.iter_rows(min_row=2,max_row=25,values_only=True):
+    val += [row]
+
+#print(val)
+print(val[5])
