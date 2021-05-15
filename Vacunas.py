434d79b8f3f2ff29ed5c7c440a8af0e7e58c3ab0
GaussGod123 <carlos_mendiola2000@hotmail.com>
2021-05-15 17:25:51 -0500

    hola

--- Vacunas.py
+++ Vacunas.py
@@ -1,33 +1,39 @@
+# Librerías necesarias para leer los datos.
+from typing import Tuple
 from openpyxl import load_workbook
 from openpyxl import Workbook
 
+# Se suben los datos y se selecciona la hoja a analizar.
 wb = load_workbook('vaccination_all_tweets.xlsx')
 sheet = wb.worksheets[0]
 
+# Se leen los títulos para conocer la localización de los elementos a buscar y se despliegan.
 tit = []
-
 for row in sheet.iter_rows(min_row=1,max_row=1,values_only=True):
 	tit += row
-
 print(tit)
-
-val = []
-
-for row in sheet.iter_rows(min_row=2,max_row=25,values_only=True):
-    val += [row]
-
-print(val[5][2])
-coor = []
-for i in range(len(val)):
-    coor.append([val[i][2]])
-print(coor)
-a = val[5][10].split(" ")
-print(a)
 print("\n")
-b = "Does"
-for i in range(len(a)):
-    if a[i] == b:
-        print("Si es igual")
+
+# Con lo anterior en mente, se leen los tweets
+tweet_range = (2,sheet.max_row)
+tweets = []
+index = 0
+for row in sheet.iter_rows(min_row=tweet_range[0], max_row=tweet_range[1], values_only=True):
+    tweets += [row]
+    print(tweets[index][10])
+    print("\n")
+    index += 1
+# coor = []
+# for i in range(len(val)):
+#     coor.append([val[i][2]])
+# print(coor)
+# a = val[5][10].split(" ")
+# print(a)
+# print("\n")
+# b = "Does"
+# for i in range(len(a)):
+#     if a[i] == b:
+#         print("Si es igual")
 
 #for i in range(len(val[5][10]))
 #print(val)
@@ -41,5 +47,6 @@ address = 'Birmingham, England'
 url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(address) +'?format=json'
 
 response = requests.get(url).json()
+print("\n")
 print(response[0]["lat"])
 print(response[0]["lon"])
