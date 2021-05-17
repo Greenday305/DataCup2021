0c4356fe41c1fad5f508b531f0125dec76a1d8ab
GaussGod123 <carlos_mendiola2000@hotmail.com>
2021-05-16 20:15:50 -0500

    Ya jalo xd

--- Vacunas copy.py
+++ Vacunas copy.py
@@ -14,10 +14,10 @@ wb = load_workbook('vaccination_all_tweets.xlsx')
 sheet = wb.worksheets[0]
 
 # The first line of the datasheet is read and the locations of the interest variables are deployed.
-tit = []
-for row in sheet.iter_rows(min_row=1, max_row=1, values_only=True):
-	tit += row
-print(tit, "\n")
+# tit = []
+# for row in sheet.iter_rows(min_row=1, max_row=1, values_only=True):
+# 	tit += row
+# #print(tit, "\n")
 
 # After analysing the data, two lists of keywords were constructed. These incude word that may be found in text and also specific hashtags.
 reactions = { "positive": ["vaccinated", "VaccinesSaveLives", "safe", "treatment", "administration", "administered", "dose", "doses", "health", "healthy", "family", "admiration", "courage", "brave", "bravery", "serious", "seriously", "merry", "merrier","Same","paste", "effective", "healthcare", "stayhome", "stayathome", "covidiots", "stayathomesavelives", "crushcovid", "sciencematters", "dignity", "health", "hopenews", "scienceovermorons", "dignityhealth", "healthcareworker", "modernavaccine", "takethevaccine", "modern", "modernmedicine", "medicine", "vaccineday", "vaccinocovid", "frontlineworkers", "trustscience", "trust", "science", "facts", "fact", "sources", "source", "information", "cases", "case", "deaths", "distribution", "specialist", "programme", "inoculation", "inoculating", "needle", "medicine", "symptoms", "available", "update", "schedule", "immunity", "authorization", "authorized", "information", "approving", "approved", "manufacture", "manufacturing"],
@@ -30,7 +30,7 @@ total_vaccines = {"pfizer": 0, "astrazeneca": 0, "sputnikv": 0, "moderna": 0, "j
 # Search of positive and negative tweets according to the keyword lists.
 rates = {"positive": 0, "negative": 0, "neutral": 0}
 retweets = {"positive": 0, "negative": 0}
-max_retweet, best_retweet, max_likes, best_liked = 0, "", 0, ""
+max_retweet, best_retweet, max_likes, best_liked, max_impact, best_impact = 0, "", 0, "", 0, ""
 today = date.today()
 
 total_words = dict()
@@ -41,15 +41,14 @@ for row in sheet.iter_rows(min_row = 2, max_row = sheet.max_row, values_only = T
     # This code line does not really solve a problem, but it makes the classification work efficiently.
     if row[11] != None : hashtags = row[11].split("'")
 
-    """ for t in dates : 
-        if t[0] == 0 : dates[t] = dates[1:]
-    day = date(",".join(dates)) """
-
     if row[13] != None : rt = int( row[13] )
-    if rt / ( today - day ).days > max_retweet : max_retweet, best_retweet = rt, row[10]
+    if rt > max_retweet : max_retweet, best_retweet = rt, row[10]
 
     if row[14] != None : lk = int( row[14] )
-    if lk / ( today - day ).days > max_likes : max_likes, best_liked = lk, row[10]
+    if lk > max_likes : max_likes, best_liked = lk, row[10]
+
+    if row[13] != None and row[14] != None : imp = ( rt + lk ) / ( today - day ).days
+    if imp > max_impact : max_impact, best_impact = imp, row[10]
 
     # Word are clasified and a global rate for the sentence is defined. The lower case convention is used in words and hashtags to avoid the consideration of repeated words with capital lettters present.
     for word in words:
@@ -81,7 +80,10 @@ for row in sheet.iter_rows(min_row = 2, max_row = sheet.max_row, values_only = T
 # Timer is terminated and results are displayed.
 end = time.time()
 
-print("Most retweets:\n", best_retweet, "\n\n", "Most liked:", best_liked, "\n")
+print("Most retweets:\n", best_retweet, "\n\n", 
+    "Most liked:\n", best_liked, "\n\n",
+    "Greatest impact:\n", best_impact, "\n\n"
+)
 
 print("Positive: ", rates["positive"], "\nRetweets: ", retweets["positive"], "\nNegative: ", rates["negative"], "\nRetweets: ",  retweets["negative"], "\nNeutral: ", rates["neutral"], "\nvaccines: ", total_vaccines, "\nTime: ", end - start)
 
@@ -90,4 +92,10 @@ plt.bar(total_vaccines.keys(), total_vaccines.values(), 1, color = 'b')
 plt.xlabel("Vacunas")
 plt.ylabel("Número de menciones")
 plt.title("Menciones de cada vacuna")
+plt.show()
+
+plt.bar(rates.keys(), rates.values(), 1, color = 'b')
+plt.xlabel("Reacciones")
+plt.ylabel("Número de tweets")
+plt.title("reacciones a la vacuna")
 plt.show()
